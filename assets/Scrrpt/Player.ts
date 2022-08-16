435165bfc6ed03035416091a3dbdca65bc907bc5
pure <1035617247@qq.commkdir>
2022-08-16 18:25:40 +0800

    U8

--- assets/Scrrpt/Player.ts
+++ assets/Scrrpt/Player.ts
@@ -1,14 +1,29 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, animation, CCObject, ConeCollider, Animation } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
+    @property(Animation)
+    Anim:Animation=null;
     start() {
 
     }
 
     update(deltaTime: number) {
         
+    }
+
+    PlayerControl(str: string) {
+        switch (str) {
+            case "Left":
+                break;
+            case "Right":
+                break
+            case "Jump":
+                break;
+            default:
+                break;
+        }
     }
 }
 
