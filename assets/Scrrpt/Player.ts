f771b36a0a1c0337e45ab8504e8d448165f2d9e3
pure <1035617247@qq.commkdir>
2022-09-07 18:18:57 +0800

    021

--- assets/Scrrpt/Player.ts
+++ assets/Scrrpt/Player.ts
@@ -2,10 +2,12 @@ import { _decorator, Component, Node, Animation, input, Input, EventKeyboard, Ke
 const { ccclass, property } = _decorator;
 @ccclass('Player')
 export class Player extends Component {
-    @property(Node)
+    AnimName:Array<string>=['Player_ider','Player_Move','Player_Jump'];
+    StatusName:Array<string>=['left','right','down','up','jump','idel'];
     Player: Node = null;
-    @property(Animation)
-    Anim: Animation = null;
+    PlayerAnim: Animation = null;
+    PlayerRigidBody2D:RigidBody2D=null;
+    UpDownTime:number=0.35;
     timeTidat:number=0;
     JumpTween: any = null;
     jumpDuration: number = 0.25;//跳跃时间
@@ -16,18 +18,27 @@ export class Player extends Component {
     IsJump: boolean = false;//是否正在跳跃
     PlayerStatus: string = "";//人物当前状态（左还是右）
     onLoad() {
-        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
-        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+       
+        this.Player=this.node;
+        this.PlayerAnim=this.Player.getComponent(Animation);
+        this.PlayerRigidBody2D=this.Player.getComponent(RigidBody2D);
+
         let collider = this.Player.getComponent(BoxCollider2D);
         if (collider) {
             collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
         }
+
+        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
     }
 
     onBeginContact(selfCollider: BoxCollider2D, otherCollider: BoxCollider2D, contact: IPhysics2DContact | null) {
         if (otherCollider.node.position.y <selfCollider.node.position.y && this.IsJump&&this.timeTidat<=0) {
             this.IsJump = false;
+            this. PlayerAnim.play("Player_ider");
             //this.Player.getComponent(Sprite).color=Color.WHITE;
+        }else if(otherCollider.node.name=='Player_g'){
+            console.log('游戏通关');
         }
     }
 
@@ -40,29 +51,31 @@ export class Player extends Component {
 
     }
 
+    //#region 控制函数
+   
     callback(event: EventTouch) {
          switch (event.currentTarget.name) {
              case "Left":
-                 this.Control("left","down");
+                 this.Control(this.StatusName[0],this.StatusName[2]);
                  break;
              case "Right":
-                 this.Control("right","down");
+                 this.Control(this.StatusName[1],this.StatusName[2]);
                  break;
              case "Jump":
-                 this.Control("jump","down");
+                 this.Control(this.StatusName[4],this.StatusName[2]);
                  break;
          }
     }
     cancelcallback(event: EventTouch) {
         switch (event.currentTarget.name) {
             case "Left":
-                this.Control("left","up");
+                this.Control(this.StatusName[0],this.StatusName[3]);
                 break;
             case "Right":
-                this.Control("right","up");
+                this.Control(this.StatusName[1],this.StatusName[3]);
                 break;
             case "Jump":
-                this.Control("jump","up");
+                this.Control(this.StatusName[4],this.StatusName[3]);
                 break;
         }
    }
@@ -72,47 +85,55 @@ export class Player extends Component {
             this.timeTidat-=deltaTime;
         }
         this.onMove(this.PlayerStatus, deltaTime);
+        if(this.IsJump){
+            // this.UpDownTime+=deltaTime;
+            // if(this.UpDownTime<0.35){
+            //     this.PlayerRigidBody2D.applyLinearImpulseToCenter(new Vec2(0, this.JumpMaxHight-=deltaTime*12), true);
+            // }else{
+            //     this.PlayerRigidBody2D.applyLinearImpulseToCenter(new Vec2(0, -1), true);
+            // }
+        }
     }
 
     Control(KeyCode:string,str:string) {
-        if(str=="down"){
+        if(str==this.StatusName[2]){
             switch (KeyCode) {
-                case "left":
-                    if (this.PlayerStatus == "right") {
+                case this.StatusName[0]:
+                    if (this.PlayerStatus == this.StatusName[1]) {
                         this.Accel = 0;
                     }
-                    this.PlayerStatus = "left";
+                    this.PlayerStatus = this.StatusName[0];
                     this.AccelDirection = -1;
                     break;
-                case "right":
-                    if (this.PlayerStatus == "left") {
+                case this.StatusName[1]:
+                    if (this.PlayerStatus == this.StatusName[0]) {
                         this.Accel = 0;
                     }
-                    this.PlayerStatus = "right";
+                    this.PlayerStatus = this.StatusName[1];
                     this.AccelDirection = 1;
                     break;
-                case "jump":
+                case this.StatusName[4]:
                     if (!this.IsJump) {
                         this.jumpAction();
                     }
                     break;
             }
         }
-        if(str=="up"){
+        if(str==this.StatusName[3]){
             switch (KeyCode) {
-                case "left":
-                    if (this.PlayerStatus == "left") {
-                        this.PlayerStatus = "Idel";
+                case this.StatusName[0]:
+                    if (this.PlayerStatus == this.StatusName[0]) {
+                        this.PlayerStatus = this.StatusName[5];
                         this.Accel = 0;
                     }
                     break;
-                case "right":
-                    if (this.PlayerStatus == "right") {
-                        this.PlayerStatus = "Idel";
+                case this.StatusName[1]:
+                    if (this.PlayerStatus == this.StatusName[1]) {
+                        this.PlayerStatus = this.StatusName[5];
                         this.Accel = 0;
                     }
                     break;
-                case "jump":
+                case this.StatusName[4]:
                     
                     break;
             }
@@ -155,37 +176,39 @@ export class Player extends Component {
 
     onMove(Status, dt) {
         switch (Status) {
-            case "right":
-            case "left":
+            case this.StatusName[0]:
+            case this.StatusName[1]:
                 if (this.Accel < this.MaxMoveSpeed) {
                     this.Accel += dt * 6;
                 }
-                if(this.IsJump&&this.Anim.getState("Player_Jump").isPlaying){
+                if(this.IsJump&&this. PlayerAnim.getState(this.AnimName[2]).isPlaying){
 
-                }else  if(!this.IsJump&&!this.Anim.getState("Player_Move").isPlaying){
-                    this.Anim.play("Player_Move");
+                }else  if(!this.IsJump&&!this. PlayerAnim.getState(this.AnimName[1]).isPlaying){
+                    this.PlayerAnim.play(this.AnimName[1]);
                 }
                 this.Player.setScale(new Vec3(this.AccelDirection * Math.abs(this.Player.scale.x), this.Player.scale.y, 1));
                 this.Player.position = this.Player.position.add3f(this.AccelDirection * (5 + (this.Accel)), 0, 0);
                 break;
-            case "Idel":
-                if(!this.IsJump&&!this.Anim.getState("Player_ider")){
-                    this.Anim.play("Player_ider");
-                }else if(!this.IsJump&&this.Anim.getState("Player_Jump")){
-                    this.Anim.play("Player_ider");
+            case "idel":
+                if(!this.IsJump&&!this. PlayerAnim.getState(this.AnimName[0])){
+                    this. PlayerAnim.play(this.AnimName[0]);
+                }else if(!this.IsJump&&this. PlayerAnim.getState(this.AnimName[2])){
+                    this. PlayerAnim.play(this.AnimName[0]);
                 }
                 break;
         }
     }
 
     jumpAction() {
-        this.timeTidat=0.1;
+        this.timeTidat=0.15;
+        this.PlayerRigidBody2D.linearVelocity=new Vec2(0,0);
+        this.UpDownTime=0;
         this.IsJump = true;
-        this.Player.getComponent(RigidBody2D).linearVelocity=new Vec2(0,0);
-        this.Player.getComponent(RigidBody2D).applyLinearImpulseToCenter(new Vec2(0, this.JumpMaxHight), true);
-        this.Anim.play("Player_Jump");
+        this.PlayerRigidBody2D.applyLinearImpulseToCenter(new Vec2(0, this.JumpMaxHight), true);
+        this. PlayerAnim.play(this.AnimName[2]);
         //this.Player.getComponent(Sprite).color=Color.RED;
     }
+     //#endregion
 
 }
 
